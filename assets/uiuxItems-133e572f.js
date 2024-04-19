31358af1347e17c6f0e5a43cb53cfd47b20d4d33
azharalshurbaji <azharalsharbaji@gmail.com>
2024-04-19 11:11:46 +0300

    new adding dist

---
+++ assets/uiuxItems-133e572f.js
@@ -0,0 +1,1 @@
+import{i as s}from"./index-9af834d7.js";const i=s("uiuxItems",{state:()=>({uiuxDetails:[]}),getters:{},actions:{showProjectDetails(t){this.uiuxDetails=t},showProjectDetails(t){this.uiuxDetails=t}}});export{i as u};
